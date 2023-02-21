28dfa43028fcb828db81580667915b12c4c12a7b
Maizares, Juan <maizaresjuan.coder@gmail.com>
2023-02-21 19:08:47 -0300

    casteo de lo retornado por axios y envio de pedido por whatsapp

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ import { GetStaticProps } from 'next';
 
 import { Product } from '../product/types';
 import api from '../product/api';
-import { Grid, Stack, Text } from '@chakra-ui/react';
+import { Button, Grid, Stack, Text } from '@chakra-ui/react';
+import Link from 'next/link';
 
 interface Props{
   products: Product[];
@@ -11,13 +12,42 @@ interface Props{
 
 const IndexRoute: React.FC<Props> = ({products}) => {
   // return <div>{JSON.stringify(products)}</div>;
-  return <Grid gridGap={6} templateColumns="repeat(auto-fill, minmax(240px, 1fr))">
-    { products.map(product => 
-      <Stack key={ product.id } backgroundColor="gray.100">
-        <Text>{ product.title }</Text>
-      </Stack>
-    )}
-  </Grid>
+  const [cart, setCart] = React.useState<Product[]>([]);
+  function handleAddToCart(product: Product){
+    setCart(cart => cart.concat(product));
+    console.log(cart);
+    
+  }
+  const text = React.useMemo(() => {
+    return cart.reduce((message, product) => message.concat(`* ${product.title} - $${product.price}\n` ), 'Boleta de compra: ')
+              .concat(`\n* Total: ${cart.reduce((total, product) => total + product.price, 0)}`);
+  }, [cart]);
+  
+  return (
+    <Stack>
+      <Grid gridGap={6} templateColumns="repeat(auto-fill, minmax(240px, 1fr))">
+        { products.map(product => 
+          <Stack key={ product.id } backgroundColor="gray.100">
+            <Text>{ product.title }</Text>
+            <Text>{ product.price }</Text>
+            <Button onClick={() => handleAddToCart(product)} colorScheme="blue">Agregar</Button>
+            
+          </Stack>
+        )}
+      </Grid>
+      { Boolean(cart.length) && (
+        
+        <Button 
+          as={Link}
+          colorScheme="whatsapp"
+          href={`https://wa.me/542217787889?text=${encodeURIComponent(text)}`}
+        >
+          Ver carrito: ({cart.length} productos.) </Button>
+      )
+      }
+        
+    </Stack>
+  )
   
 }
 
