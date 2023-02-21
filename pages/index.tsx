b07e1bb1f38b11511f7a621eea6cc542bfaf6f04
Maizares, Juan <maizaresjuan.coder@gmail.com>
2023-02-21 16:43:05 -0300

    agregado de estructura y estilos con Chakra

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { GetStaticProps } from 'next';
+
 import { Product } from '../product/types';
 import api from '../product/api';
+import { Grid, Stack, Text } from '@chakra-ui/react';
+
 interface Props{
   products: Product[];
 }
 
 const IndexRoute: React.FC<Props> = ({products}) => {
-  return <div>{JSON.stringify(products)}</div>;
-
+  // return <div>{JSON.stringify(products)}</div>;
+  return <Grid gridGap={6} templateColumns="repeat(auto-fill, minmax(240px, 1fr))">
+    { products.map(product => 
+      <Stack key={ product.id } backgroundColor="gray.100">
+        <Text>{ product.title }</Text>
+      </Stack>
+    )}
+  </Grid>
   
 }
 
